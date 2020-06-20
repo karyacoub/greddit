9b3ad421e2ba6bb45b5de3f4d24d5f805363110a
karyacoub <karyacoub@gmail.com>
2020-06-19 21:52:58 -0500

    Test push

--- src/components/home-screen/PostListing.tsx
+++ src/components/home-screen/PostListing.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import { IListing } from "../../models/Listing.model";
 import styles from "../../styles/HomeScreen.styles";
 import { StyledText } from "../common/StyledText";
